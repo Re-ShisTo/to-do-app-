b0c020fdb91c81704bd4b73ec2c9bff12e4441c6
Mango God <anil7053r@gmail.com>
2026-02-03 20:36:08 +0600

    ALL UI functionality is set up completely

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,75 @@ const App = () => {
   const [todoList, setTodoList] = useState([]);
   const [currentFilter, setCurrentFilter] = useState("all");
 
+  const getAllTodos = async () => {
+    const API = `http://localhost:3000/todo`;
+    let url = API;
+
+    if (currentFilter === "active") {
+      url += `?isComplete=false`;
+    } else if (currentFilter === "completed") {
+      url += "?isComplete=true";
+    }
+
+    const res = await fetch(url);
+    const data = await res.json();
+    setTodoList(data);
+  };
+
   useEffect(() => {
-    const getAllTodos = async () => {
-      const API = `http://localhost:3000/todo`;
-      const url = API;
-      if (currentFilter === "active") {
-        url;
-      }
-      const res = await fetch(`http://localhost:3000/todo`);
-      const data = await res.json();
-      setTodoList(data);
+    // eslint-disable-next-line react-hooks/set-state-in-effect
+    getAllTodos();
+  }, [currentFilter]);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (!todoTitle.trim()) {
+      return alert("Please provide a valid todo title");
+    }
+
+    const newTodo = {
+      title: todoTitle,
+      isComplete: false,
     };
 
+    await fetch(`http://localhost:3000/todo`, {
+      method: "POST",
+      body: JSON.stringify(newTodo),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
     getAllTodos();
-  }, []);
+    setTodoTitle("");
+  };
+
+  const toggleIsComplete = async (todo) => {
+    await fetch(`http://localhost:3000/todo/${todo.id}`, {
+      method: "PATCH",
+      body: JSON.stringify({ isComplete: !todo.isComplete }),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+
+    getAllTodos();
+  };
+
+  const deleteHandler = async (todoId) => {
+    await fetch(`http://localhost:3000/todo/${todoId}`, {
+      method: "DELETE",
+    });
+
+    getAllTodos();
+  };
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow">
       <h2 className="text-2xl font-semibold text-center mb-6">
         Connecting APIs with our components
       </h2>
-      <form className="flex gap-2 mb-4">
+      <form className="flex gap-2 mb-4" onSubmit={submitHandler}>
         <input
           type="text"
           value={todoTitle}
@@ -58,18 +106,22 @@ const App = () => {
             className="flex items-center justify-between p-3
            border rounded-md"
           >
-            <div>
+            <div className="w-full flex items-center justify-between">
               <input
                 type="checkbox"
-                checked={todo.isCompleted}
+                checked={todo.isComplete}
+                onChange={() => toggleIsComplete(todo)}
                 className="h-4 w-4"
               />
               <span
-                className={`${todo.isCompleted ? "line-through text-gray-400" : " text-gray-800"}`}
+                className={`${todo.isComplete ? "line-through text-gray-400" : " text-gray-800"}`}
               >
                 {todo.title}
               </span>
-              <button className="text-sm text-red-400 hover:text-red-800">
+              <button
+                className="text-sm text-red-400 hover:text-red-800"
+                onClick={() => deleteHandler(todo.id)}
+              >
                 Delete
               </button>
             </div>
